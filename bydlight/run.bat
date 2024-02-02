a227773fb102f3d30d020399eeadc042b5d649d0
johntime <johntime163@163.com>
2024-02-02 10:36:47 +0800

    上传服务端

---
+++ bydlight/run.bat
@@ -0,0 +1,6 @@
+REM Forge requires a configured set of both JVM and program arguments.
+REM Add custom JVM arguments to the user_jvm_args.txt
+REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
+REM  pass them to this script directly
+"C:\Program Files\Java\jdk-17\bin\java.exe" @user_jvm_args.txt -javaagent:authlib-injector-1.2.4.jar=https://littleskin.cn/api/yggdrasil @libraries/net/minecraftforge/forge/1.18.2-40.2.0/win_args.txt %*
+pause
