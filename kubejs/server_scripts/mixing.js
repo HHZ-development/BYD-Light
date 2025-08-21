78dd39f84bc5d90fa3b65edb4b3bd522f2e23ca4
HHZwty <wty2007315@outlook.com>
2025-08-21 21:16:00 +0800

    mix

--- kubejs/server_scripts/mixing.js
+++ kubejs/server_scripts/mixing.js
@@ -17,19 +17,16 @@
     }).id('kubejs:mixing/steel_dust')
  //高效玫瑰石英   
     event.custom({
-        type: 'create:mixing',
-        ingredients: [
-            { tag: 'ae2:all_quartz' },
-            { tag: 'ae2:all_quartz' },
-            { item: 'minecraft:redstone' },
-            { item: 'minecraft:redstone' },
-            { item: 'minecraft:redstone' },
-            { item: 'minecraft:redstone' }
-        ],
-        results: [
-            {
-                id: '2x create:rose_quartz'
-            }
-        ]
-    }).id('kubejs:mixing/rose_quartz')
+  type: 'create:mixing',
+  ingredients: [
+    { item: 'minecraft:quartz' },
+    { item: 'minecraft:redstone' },
+    { item: 'minecraft:redstone' }
+  ],
+  result: [
+    {
+      item: 'create:rose_quartz'
+    }
+  ]
+}).id('kubejs:mixing/rose_quartz')
 })
