5f947553260305117c6fc615f38e89b7132cf8e2
HHZwty <wty2007315@outlook.com>
2025-08-23 17:48:15 +0800

    删除电路板配方

--- kubejs/server_scripts/common.js
+++ kubejs/server_scripts/common.js
@@ -8,18 +8,7 @@ ServerEvents.recipes(event => {
             B: { item: 'create:zinc_ingot' },
             C: { item: 'minecraft:copper_ingot' },
             D: { item: 'mekanism:alloy_infused' }
-    }).id('kubejs:test/refined_precision_alloy')
- // 基础电路板 basic_circuit    
-    event.shaped('kubejs:basic_circuit', [
-            'ADA',
-            'CBC',
-            'ADA'
-    ], {
-            A: { item: 'mekanism:ingot_steel' },
-            B: { item: 'mekanism:basic_control_circuit' },
-            C: { item: 'createaddition:copper_wire' },
-            D: { item: 'create:iron_sheet' }
-    }).id('kubejs:test/basic_circuit')  
+    }).id('kubejs:test/refined_precision_alloy') 
  // 强化黄铜锭
     event.shaped('kubejs:reinforced_brass_ingot', [
         'ABA',
